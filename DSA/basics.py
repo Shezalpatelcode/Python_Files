8efb78249af7793da913f620ebbb86fb6d2f86d5
Shezalpatelcode <sheezalp88@gmail.com>
2025-11-04 23:45:36 +0530

    Python Programs

---
+++ DSA/basics.py
@@ -0,0 +1,17 @@
+lst =['one','two','three','four']
+
+lst.reverse()
+print (lst)
+
+#to print if the condition numbrt is there in both the set
+if 'two' in lst:
+    print('AI')
+if 'six'  not in lst:
+    print('ML')   
+#------------------to print the ssorted list ----------------------------------
+#it always happen in ascending
+
+number=[1,2,3,6,8]
+sorted_lst=sorted(number)
+
+# reverse sorted order
