4e8526ec1de55ddda613ba3df85f60ff8ef9136e
Shezalpatelcode <sheezalp88@gmail.com>
2025-11-24 19:31:11 +0530

    Python Files

---
+++ tuple.py
@@ -0,0 +1,43 @@
+# t=(1,3,4)
+# print(t)
+# tup = (1, 2, 3, 4, 5)
+
+# # tuples are indexed
+# print(tup[1])
+# print(tup[4])
+
+# # tuples contain duplicate elements
+# tup = (1, 2, 3, 4, 2, 3)
+# print(t)
+
+# # updating an element
+# # tup[1] = 100
+# # print(tup)
+# tupl=('flower',)
+# type(tupl)
+# print(type)
+
+# tap=('satish','murali','naveen','srinu','brahma')
+# print(tap[1])
+# print(tap[-1])
+
+# # -----------------------------------
+# tab=('ABC',('satish','naveen','srinu'))
+# print(tab[1])
+# print(tab[1][2])
+
+# #------------------------------------
+# tub=(1,2,3,4,5,6)
+# print(tub[1:4])
+# print(tub[:-2])
+# print(tub[:])
+# # tub[2]='12'
+# tun=(1,2,3,4,[5,6,7])
+# # tun[4][1]='satish'
+# print(tun)
+# add=(1,2,3)+(4,5,6)
+# print(add)
+t=(2,5,1,6,9)
+print(max(t))
+print(min(t))
+print(sum(t))
